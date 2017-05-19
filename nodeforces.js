b3572a5ff101e1c6fc11efa512029db8e1bd8e55
kp96 <krishnakalubandi@gmail.com>
2017-05-19 21:21:38 +0530

    restore nodeforces.js

--- nodeforces.js
+++ nodeforces.js
@@ -1,24 +1,13 @@
 /**
  * Main api that exposes init and test functions
  */
-var testapi = require('./lib/testapi');
 
-// module.exports = {
-//     init: function(args, cb) {
-//         return cb();
-//     },
-//
-//     test: function(args) {
-//         var testRunner = testapi.getRunner(),
-//             testInstance = testapi.getTestInstance(testRunner, 'simple test');
-//
-//         testapi.addTest(testInstance, 'first case', ['toutput0.txt', 'output0.txt']);
-//         testapi.addTest(testInstance, 'first case', ['toutput1.txt', 'output1.txt']);
-//
-//         testapi.run(testRunner);
-//     }
-// };
+module.exports = {
+    init: function(args, cb) {
+        return cb();
+    },
 
-testapi.runTests('765A', '/home/krishna/Documents/projects/test', function(err) {
-    err && console.log(error);
-});
+    test: function(args, cb) {
+        return cb();
+    }
+};
