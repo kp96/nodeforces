92e0b58667951b62669d84556685b4d76cf017c5
kp96 <krishnakalubandi@gmail.com>
2017-05-19 21:19:04 +0530

    added test api functions

--- nodeforces.js
+++ nodeforces.js
@@ -1,13 +1,24 @@
 /**
  * Main api that exposes init and test functions
  */
+var testapi = require('./lib/testapi');
 
-module.exports = {
-    init: function(args, cb) {
-        return cb();
-    },
+// module.exports = {
+//     init: function(args, cb) {
+//         return cb();
+//     },
+//
+//     test: function(args) {
+//         var testRunner = testapi.getRunner(),
+//             testInstance = testapi.getTestInstance(testRunner, 'simple test');
+//
+//         testapi.addTest(testInstance, 'first case', ['toutput0.txt', 'output0.txt']);
+//         testapi.addTest(testInstance, 'first case', ['toutput1.txt', 'output1.txt']);
+//
+//         testapi.run(testRunner);
+//     }
+// };
 
-    test: function(args, cb) {
-        return cb();
-    }
-};
+testapi.runTests('765A', '/home/krishna/Documents/projects/test', function(err) {
+    err && console.log(error);
+});
