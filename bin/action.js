8c8010d7bfc4c8f9e3c87a583feabbe80798d16c
kp96 <krishnakalubandi@gmail.com>
2017-05-28 13:06:43 +0530

    simplify parsing

---
+++ bin/action.js
@@ -0,0 +1,38 @@
+/**
+ * CLI Parser for initialize command
+ */
+
+require('colors');
+
+var async = require('async'),
+    ora = require('ora');
+
+
+module.exports = function() {
+    var args = arguments || {},
+        action = args['1']._name,
+        loaderText = action === 'init' ? 'Parsing problem from codeforces' : 'Compiling and executing your code',
+        spinner = ora(loaderText).start(),
+        taskFn = require('../')[action];
+
+    async.waterfall([
+        // 1. Call action method
+        function(cb) {
+            return taskFn(args, cb);
+        }
+
+    ], function(err) {
+
+        if (err) {
+            spinner.fail(err.toString().red);
+            return process.exit(1);
+        }
+
+        var successResponse = {
+            init: `File Created at ${args.filePath}. Get ready to start coding`.green,
+            test: 'Reporting Test Results'
+        };
+
+        return spinner.succeed(successResponse[action]);
+    });
+};
